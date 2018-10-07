5d6ef155b37d4e1c869e371b677c091d6daf766b
ChristineMWilde <chrisk_21@yahoo.co.uk>
2018-10-07 10:33:22 +0100

    initial commit

---
+++ settings/dev.py
@@ -0,0 +1,22 @@
+from base import *
+ 
+DEBUG = True
+ 
+INSTALLED_APPS.append('debug_toolbar')
+
+MIDDLEWARE.append('debug_toolbar.middleware.DebugToolbarMiddleware')
+ 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+#Disqus
+DISQUS_WEBSITE_SHORTNAME = 'codeinstitutesocialstaging'
+ 
+# Stripe environment variables
+STRIPE_PUBLISHABLE = os.getenv('STRIPE_PUBLISHABLE', 'pk_test_huXVMABXPzJmtBClroUo7vFA')
+STRIPE_SECRET = os.getenv('STRIPE_SECRET', 'sk_test_qgkn8rHXvHGEtPaqreOCeXi7')
+ 
